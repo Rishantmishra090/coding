61d9bbff9447088b497ac7908ce0fcfcd599ff33
Rishantmishra090 <158550133+Rishantmishra090@users.noreply.github.com>
2024-02-02 12:10:50 +0530

    Add files via upload

---
+++ arithmetic operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+int n1,n2:
+cout<<"enter two no";
+cin>>n1>>n2;
+cout<<"sum is"<<n1+n2;
+cout<<"diff is"<<n1-n2;
+cout<<"product is"<<num1*num2;
+cout<<"quotient is"<<num1/num2;
+cout<<"remainderis"<<num1*num2;
+return 0;
+}
